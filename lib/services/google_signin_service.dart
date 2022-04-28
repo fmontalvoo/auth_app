81350b4daf6d9d34bb4f588545eebc2f57ddaf9a
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-04-27 22:48:08 -0500

    Se valida token de Google contra el backend.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- lib/services/google_signin_service.dart
+++ lib/services/google_signin_service.dart
@@ -1,5 +1,7 @@
 import 'dart:developer';
 
+import 'package:http/http.dart' as http;
+
 import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleSigninService {
@@ -15,8 +17,25 @@ class GoogleSigninService {
       final account = await googleSignIn.signIn();
       final gooleKey = await account?.authentication;
       log(account.toString());
-      log('Token ID: ${gooleKey?.idToken}');
-      return account;
+      final token = gooleKey?.idToken;
+      log('Token ID: $token');
+
+      final signinApi = Uri(
+        scheme: 'https',
+        host: 'auth-app-backend-07.herokuapp.com',
+        path: '/auth/google_signin',
+      );
+
+      final session = await http.post(signinApi, body: {'token': token});
+
+      if (session.statusCode == 200) {
+        log('Signin Success');
+        log(session.body);
+        return account;
+      } else {
+        log('Signin Failed');
+        return null;
+      }
     } catch (e) {
       log('Error: $e');
     }
